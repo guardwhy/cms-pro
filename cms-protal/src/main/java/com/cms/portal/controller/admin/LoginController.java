678804735fad3fdc919240a0447fe695168744f6
guardwhy <hxy1625309592@aliyun.com>
2022-02-27 20:48:42 +0800

    整合kaptcha,拿到验证码

--- cms-protal/src/main/java/com/cms/portal/controller/admin/LoginController.java
+++ cms-protal/src/main/java/com/cms/portal/controller/admin/LoginController.java
@@ -1,7 +1,15 @@
 package com.cms.portal.controller.admin;
 
+import com.google.code.kaptcha.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * @author guardwhy
@@ -9,8 +17,30 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class LoginController {
+
+    // 注入Producer
+    @Autowired
+    private Producer captchaProducer;
+
     @GetMapping("login.do")
+    // 登录页面
     public String toLogin(){
         return "/admin/login";
+    }
+
+    /***
+     * 拿到验证码
+     * @param httpServletResponse
+     */
+    @GetMapping("captcha.do")
+    public void doCaptcha(HttpServletResponse httpServletResponse){
+        String text = captchaProducer.createText();
+        // 拿到图像
+        BufferedImage image = captchaProducer.createImage(text);
+        try(ServletOutputStream outputStream = httpServletResponse.getOutputStream()){
+            ImageIO.write(image, "jpg", outputStream);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
