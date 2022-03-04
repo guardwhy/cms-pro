278218de896332726c82b9b0ab047e4ef462bd31
guardwhy <hxy1625309592@aliyun.com>
2022-03-04 13:59:46 +0800

    Layui封装和登录功能实现1

--- cms-protal/src/main/java/com/cms/portal/security/filter/CmsAuthenticationFilter.java
+++ cms-protal/src/main/java/com/cms/portal/security/filter/CmsAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.cms.portal.security.filter;
 
+import com.alibaba.fastjson.JSON;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 
 import javax.servlet.ServletRequest;
@@ -10,10 +11,22 @@ import javax.servlet.ServletResponse;
  * @date 2022/3/3 15:30
  */
 public class CmsAuthenticationFilter extends FormAuthenticationFilter {
+    // 设置登录前置路径
+    private static final String ADMIN_LOGIN = "/admin/cms/login.do";
+
     @Override
     // 判断是前台登录或者是后台登录请求
     protected boolean isLoginRequest(ServletRequest request, ServletResponse response) {
         return this.pathsMatch(this.getLoginUrl(), request) ||
-                this.pathsMatch("/admin/cms/login.do", request);
+                this.pathsMatch(ADMIN_LOGIN, request);
+    }
+
+    @Override
+    // 响应登录JSON
+    protected boolean executeLogin(ServletRequest request, ServletResponse response) throws Exception {
+       response.setCharacterEncoding("utf-8");
+       response.setContentType("application/json;charset=UTF-8");
+       response.getWriter().write(JSON.toJSONString("密码错误！！！"));
+       return true;
     }
 }
