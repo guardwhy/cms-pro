76c7c228288d36c173202eeba9e07d699b8d6c7f
guardwhy <hxy1625309592@foxmail.com>
2022-03-22 15:52:26 +0800

    页面登录跳转首页

--- cms-protal/src/main/java/com/cms/portal/security/filter/CmsUserFilter.java
+++ cms-protal/src/main/java/com/cms/portal/security/filter/CmsUserFilter.java
@@ -3,6 +3,11 @@ package com.cms.portal.security.filter;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.shiro.web.filter.authc.UserFilter;
+import org.apache.shiro.web.util.WebUtils;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author guardwhy
@@ -15,4 +20,17 @@ import org.apache.shiro.web.filter.authc.UserFilter;
 public class CmsUserFilter extends UserFilter {
     private String  adminLoginUrl;
     private String adminPrefix;
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest req, ServletResponse response) throws Exception {
+        // 拿到HttpServletRequest
+        HttpServletRequest request = (HttpServletRequest) req;
+        // 保存request
+        this.saveRequest(request);
+        // 获取requestURI
+        String requestURI = request.getRequestURI();
+        // 跳转
+        WebUtils.issueRedirect(request, response, (requestURI.startsWith(adminPrefix)) ? adminLoginUrl: this.getLoginUrl());
+        return false;
+    }
 }
