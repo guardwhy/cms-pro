76ea8dfdbd703e4ff2beed69db22b4b7a8f81a53
guardwhy <hxy1625309592@foxmail.com>
2022-03-24 16:44:06 +0800

    显示权限下拉树dtree

--- cms-protal/src/main/java/com/cms/portal/controller/admin/PermissionController.java
+++ cms-protal/src/main/java/com/cms/portal/controller/admin/PermissionController.java
@@ -7,6 +7,9 @@ import com.cms.core.annotation.DoValid;
 import com.cms.dao.enums.PermissionTypeEnum;
 import com.cms.service.api.CmsPermissionService;
 import com.cms.service.dto.CmsPermissionDto;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.apache.shiro.util.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +18,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author guardwhy
@@ -49,5 +56,68 @@ public class PermissionController {
         cmsPermissionService.save(cmsPermissionDto);
         // 返回成功
         return Result.success();
+    }
+
+    @PostMapping("selectTree.do")
+    @ResponseBody
+    public Result doSelectTree(){
+        List<CmsPermissionDto> cmsPermissionDtos = buildData();
+        // 存放所有数据
+        Map<Integer, CmsPermissionDto> permissionMap = Maps.newHashMap();
+        // 只存放parentId = 0的数据
+        List<CmsPermissionDto> permissionList = Lists.newArrayList();
+        // 循环数据，进行处理
+        cmsPermissionDtos.forEach(x->{
+            // 拿到id值
+            Integer id = x.getId();
+            // 放入Map集合中
+            permissionMap.put(id, x);
+            // 获取当前dto的父类id
+            Integer parentId = x.getParentId();
+            // 判断当前是否是顶级菜单
+            if(parentId == 0){
+                permissionList.add(x);
+            }else{
+                CmsPermissionDto cmsPermissionDto = permissionMap.get(parentId);
+                List<CmsPermissionDto> children = cmsPermissionDto.getChildren();
+                if(CollectionUtils.isEmpty(children)){
+                    children = Lists.newArrayList();
+                }
+                // 子类添加操作
+                children.add(x);
+                cmsPermissionDto.setChildren(children);
+            }
+        });
+        return Result.success((ArrayList)permissionList);
+    }
+
+    public List<CmsPermissionDto> buildData(){
+        List<CmsPermissionDto> permissionList = Lists.newArrayList();
+        //4条数据
+        CmsPermissionDto cmsPermissionDto1 = new CmsPermissionDto();
+        CmsPermissionDto cmsPermissionDto2 = new CmsPermissionDto();
+        CmsPermissionDto cmsPermissionDto3 = new CmsPermissionDto();
+        CmsPermissionDto cmsPermissionDto4 = new CmsPermissionDto();
+
+        cmsPermissionDto1.setId(1);
+        cmsPermissionDto2.setId(2);
+        cmsPermissionDto3.setId(3);
+        cmsPermissionDto4.setId(4);
+
+        cmsPermissionDto1.setName("测试1");
+        cmsPermissionDto2.setName("测试2");
+        cmsPermissionDto3.setName("测试3");
+        cmsPermissionDto4.setName("测试4");
+
+        cmsPermissionDto1.setParentId(0);
+        cmsPermissionDto2.setParentId(1);
+        cmsPermissionDto3.setParentId(2);
+        cmsPermissionDto4.setParentId(3);
+
+        permissionList.add(cmsPermissionDto1);
+        permissionList.add(cmsPermissionDto2);
+        permissionList.add(cmsPermissionDto3);
+        permissionList.add(cmsPermissionDto4);
+        return permissionList;
     }
 }
