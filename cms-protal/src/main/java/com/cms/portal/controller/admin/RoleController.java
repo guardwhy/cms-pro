ad0031ffb9ac8788b81e6dac4023391a09505657
guardwhy <hxy1625309592@foxmail.com>
2022-04-02 09:51:56 +0800

    角色管理添加

--- cms-protal/src/main/java/com/cms/portal/controller/admin/RoleController.java
+++ cms-protal/src/main/java/com/cms/portal/controller/admin/RoleController.java
@@ -1,7 +1,13 @@
 package com.cms.portal.controller.admin;
 
+import com.cms.context.foundation.Result;
+import com.cms.context.utils.UtilsTemplate;
+import com.cms.service.dto.CmsRoleDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author guardwhy
@@ -11,4 +17,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("role")
 public class RoleController {
 
+    /***
+     * 拿到角色index
+     * @return
+     */
+    @GetMapping("index.do")
+    public String toIndex(){
+        return UtilsTemplate.adminTemplate("role", "index");
+    }
+
+    @PostMapping("page.do")
+    @ResponseBody
+    // 请求数据
+    public Result doPage(CmsRoleDto cmsRoleDto){
+        return Result.success();
+    }
 }
